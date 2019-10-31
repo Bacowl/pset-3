7494ea980d695893ede64109c4babb9ea8d83bd9
Benjamin Carpenter <BCarpenter@ucvts.tec.nj.us>
2019-10-31 13:21:48 -0400

    second commit

--- src/gpa.js
+++ src/gpa.js
@@ -2,7 +2,33 @@ const readlineSync = require("readline-sync");
 
 let grade = readlineSync.question("\nEnter a letter grade: ");
 
-if (grade ==  "A") {
-    console.log("noice")
-} else if (grade == "B")
-    console.log("less noice")
+if (grade ==  "A" || grade == "A+") {
+    var gradeNumber = "4.00"
+}  else if (grade == "B") {
+    var gradeNumber = "3.00"
+}  else if (grade == "C") {
+    var gradeNumber = "2.00"
+}  else if (grade == "D") {
+    var gradeNumber = "1.00"
+}  else if (grade == "F") {
+    var gradeNumber = "0.00"
+}  else if (grade == "A-") {
+    var gradeNumber = "3.66"
+}  else if (grade == "B+") {
+    var gradeNumber = "3.33"
+}  else if (grade == "B-") {
+    var gradeNumber = "2.66"
+}  else if (grade == "C+") {
+    var gradeNumber = "2.33"
+}  else if (grade == "C-") {
+    var gradeNumber = "1.66"
+}  else if (grade == "D+") {
+    var gradeNumber = "1.33"
+}  else if (grade == "D-") {
+    var gradeNumber = "0.66"
+}  else {
+  console.log("\nInvalid")
+}
+if (gradeNumber != 3.14) {
+console.log("Your GPA is "+gradeNumber+".")
+}
