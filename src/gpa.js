db43ebb3a62f4f7edf95e8f306a4b602e268a8f0
Bacowl <44117000+Bacowl@users.noreply.github.com>
2019-10-31 14:36:50 -0400

    third commit

--- src/gpa.js
+++ src/gpa.js
@@ -2,33 +2,47 @@ const readlineSync = require("readline-sync");
 
 let grade = readlineSync.question("\nEnter a letter grade: ");
 
+let valid = 0
+
 if (grade ==  "A" || grade == "A+") {
     var gradeNumber = "4.00"
+    valid++
 }  else if (grade == "B") {
     var gradeNumber = "3.00"
+    valid++
 }  else if (grade == "C") {
     var gradeNumber = "2.00"
+    valid++
 }  else if (grade == "D") {
     var gradeNumber = "1.00"
+    valid++
 }  else if (grade == "F") {
     var gradeNumber = "0.00"
+    valid++
 }  else if (grade == "A-") {
     var gradeNumber = "3.66"
+    valid++
 }  else if (grade == "B+") {
     var gradeNumber = "3.33"
+    valid++
 }  else if (grade == "B-") {
     var gradeNumber = "2.66"
+    valid++
 }  else if (grade == "C+") {
     var gradeNumber = "2.33"
+    valid++
 }  else if (grade == "C-") {
     var gradeNumber = "1.66"
+    valid++
 }  else if (grade == "D+") {
     var gradeNumber = "1.33"
+    valid++
 }  else if (grade == "D-") {
     var gradeNumber = "0.66"
+    valid++
 }  else {
   console.log("\nInvalid")
 }
-if (gradeNumber != 3.14) {
+if (valid == 1) {
 console.log("Your GPA is "+gradeNumber+".")
 }
