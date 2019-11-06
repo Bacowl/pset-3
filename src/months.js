e01e7ad14267e58ffae73facab0b3bb09148b16f
Benjamin Carpenter <BCarpenter@ucvts.tec.nj.us>
2019-11-06 13:12:58 -0500

    one more commit

--- src/months.js
+++ src/months.js
@@ -1,3 +1,15 @@
 const readlineSync = require("readline-sync");
 
-let card = readlineSync.question("\nEnter a playing card: ");
+let month = readlineSync.question("\nEnter a month: ");
+let monthCap = month.toUpperCase()
+
+if (monthCap.includes("JAN")||monthCap.includes("MAR")||monthCap.includes("MAY")||monthCap.includes("JUL")||monthCap.includes("AUG")||monthCap.includes("OCT")||monthCap.includes("DEC")) {
+var days = "31 days."
+} else if (monthCap.includes("JAN")||monthCap.includes("JAN")||monthCap.includes("JAN")||monthCap.includes("JAN")) {
+var days = "30 days."
+} else if (monthCap.includes("FEB")) {
+var days = "28 or 29 days."
+} else {
+var days = "Invalid."
+}
+console.log("\n"+days)
